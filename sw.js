18d7ce65431ebe0d85c77ed9ba1ae406eee85fc4
Dimitris <81325135+tokenno@users.noreply.github.com>
2025-06-10 16:42:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'sly-app-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
+  '/risk/',
+  '/risk/index.html',
   '/icon-512x512.png',
   '/icon-192x192.png',
   '/icon-512-maskable.png',
